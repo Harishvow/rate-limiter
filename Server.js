a7308c53414d495d1be8b435d943ad75e064872e
Harishvow <harisharish982005@gmail.com>
2026-02-10 20:07:08 +0530

    role based logic implement  ongoing

--- Server.js
+++ Server.js
@@ -5,16 +5,17 @@ import { tokenBucketLimiter } from "./Algorthims/TokenBucket.js";
 const app = express();
 
 app.use(tokenBucketLimiter({
-  capacity: 10,
-  refillRatePerSec:5
+  capacity: 5,
+  refillRatePerSec:3
+
 }));
 
 
-app.get("/api/hello",(req,res)=>{
+app.get("/api",(req,res)=>{
     res.json({ok:true,message:"hello world "})
 });
 
 const port=process.env.PORT ||3000;
 app.listen(port, () => {
-  console.log(`✅ Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
